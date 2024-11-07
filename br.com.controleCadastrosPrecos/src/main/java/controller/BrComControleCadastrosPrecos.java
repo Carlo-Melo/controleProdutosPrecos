e25449ad52f28b0cf1f6401489b2958661983585
Carlos Eduardo Melo <133800041+Carlo-Melo@users.noreply.github.com>
2024-11-07 12:23:40 -0300

    Add files via upload

---
+++ br.com.controleCadastrosPrecos/src/main/java/controller/BrComControleCadastrosPrecos.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class BrComControleCadastrosPrecos {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
